100dea2d1baf7a23674ab5ff45930748dcb92ff6
Acosta Gustavo <gustavo080487@gmail.com>
2024-05-25 11:01:43 -0300

    config_apiRouter

--- src/routers/api.router.js
+++ src/routers/api.router.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
-import { authRouter } from "./api/auth.router.js";
-import { apiErrorHandler } from "../middlewares/apiErrorHandler.js";
-import { userRouter } from "./api/users.router.js";
-import { gastronomyRouter } from "./api/gastronomy.router.js";
+import { Router } from "express"
+import { authRouter } from "./api/auth.router.js"
+import { apiErrorHandler } from "../middlewares/apiErrorHandler.js"
+import { userRouter } from "./api/users.router.js"
+import { gastronomyRouter } from "./api/gastronomy.router.js"
 
 export const apiRouter = Router()
 
