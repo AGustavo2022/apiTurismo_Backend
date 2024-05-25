5183b4d22f6144ae374a24a5b8914aa73b3d9b09
Acosta Gustavo <gustavo080487@gmail.com>
2024-05-25 11:26:51 -0300

    config_cors

--- src/app/app.js
+++ src/app/app.js
@@ -4,14 +4,14 @@ import morgan from "morgan"
 import cookieParser from 'cookie-parser'
 import { COOKIE_SECRET } from "../config/auth.config.js"
 import { extraerToken } from "../middlewares/authentication.middleware.js"
-// import  cors  from "cors";
+import  cors  from "cors";
 
 export const app = express()
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true,
-// }))
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
